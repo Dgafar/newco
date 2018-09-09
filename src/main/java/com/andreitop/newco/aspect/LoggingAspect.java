5e0342f1e36255ba327be298aecadf02289c66c8
Dgafar <nithogg@rambler.ru>
2018-09-09 18:38:59 +0300

    Task aop finished

--- src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
+++ src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
@@ -4,9 +4,10 @@ package com.andreitop.newco.aspect;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+import java.util.concurrent.TimeUnit;
 
 @Aspect
 @Component
@@ -14,12 +15,28 @@ public class LoggingAspect {
 
     private static final Logger logger = LogManager.getLogger(LoggingAspect.class);
 
-
     @Before("com.andreitop.newco.aspect.PointcutContainer.repositoryFind()")
     public void beforeRepoFind(JoinPoint joinPoint) {
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
         logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
+    }
+
+    @AfterReturning("com.andreitop.newco.aspect.PointcutContainer.repositorySave()")
+    public void afterRepoCreate(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " create object");
+    }
+
+    @Around("com.andreitop.newco.aspect.PointcutContainer.repositoryAllMethods()")
+    public Object measureMethodExecutionTime(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.nanoTime();
+        Object retval = pjp.proceed();
+        long end = System.nanoTime();
+        String methodName = pjp.getSignature().getName();
+        logger.info("Execution of " + methodName + " took " + TimeUnit.NANOSECONDS.toMillis(end - start) + " ms");
+        return retval;
     }
 
 }
--- src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
+++ src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
@@ -4,7 +4,13 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class PointcutContainer {
 
-    @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.find*())")
+    @Pointcut("execution(* *com.andreitop.newco.repository.*Repo*.find*())")
     public void repositoryFind() {
     }
+
+    @Pointcut("execution( * *save*(..))")
+    public void repositorySave() {}
+
+    @Pointcut("within(com.andreitop.newco.repository.*)")
+    public void repositoryAllMethods() {}
 }
